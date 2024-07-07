9c634397109af515733e854313db847a32ddf7cb
Avasarala Sai Kiran <58164887+asaikiran1999@users.noreply.github.com>
2024-07-08 01:38:44 +0530

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,1 @@
-from app import app
 
-if __name__ == '__main__':
-    # debug = False
-    debug = True
-    #clustering('2022-01-03 00:00:00' , 5)
-    #print(df4.head())
-    #show_agent_shedule()
-    app.run(debug=debug)
